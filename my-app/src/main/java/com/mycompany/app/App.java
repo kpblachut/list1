a28e0cf71d904ee954de1f8ae17bb3ed20514da0
kpblachut <kpblachut@gmail.com>
2023-10-18 15:38:25 +0200

    new circle

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,8 @@ public class App
         Circle circle = new Circle(5);
         out.println("Circumference of the circle is equal to " + circle.getCircumference());
 
+        Circle circle1 = new Circle(123);
+
         Square square = new Square(12);
         out.println("Area of the square is equal to " + square.getField());
     }
